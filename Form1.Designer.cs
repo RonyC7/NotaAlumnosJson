63f74c43b1be4fcdd4e484b7b6bf9e0f1ff40331
RonyC7 <carpioalvaradorony@mesoamericana.edu.gt>
2024-04-01 08:22:30 -0600

    Primer Diseño terminado (Falta programacion)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,170 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridViewDatos = new System.Windows.Forms.DataGridView();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.comboBoxCursos = new System.Windows.Forms.ComboBox();
+            this.textBoxNotas = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buttonGuardar = new System.Windows.Forms.Button();
+            this.buttonMostrar = new System.Windows.Forms.Button();
+            this.buttonEliminar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewDatos
+            // 
+            this.dataGridViewDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewDatos.Location = new System.Drawing.Point(28, 265);
+            this.dataGridViewDatos.Name = "dataGridViewDatos";
+            this.dataGridViewDatos.RowHeadersWidth = 51;
+            this.dataGridViewDatos.RowTemplate.Height = 24;
+            this.dataGridViewDatos.Size = new System.Drawing.Size(892, 150);
+            this.dataGridViewDatos.TabIndex = 0;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(149, 42);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(275, 22);
+            this.textBoxNombre.TabIndex = 1;
+            // 
+            // comboBoxCursos
+            // 
+            this.comboBoxCursos.FormattingEnabled = true;
+            this.comboBoxCursos.Items.AddRange(new object[] {
+            "Matematicas",
+            "Fisica",
+            "Programacion",
+            "Algebra",
+            "Quimica",
+            "Estructura de datos",
+            "Compiladores",
+            "Automatas y lenguajes"});
+            this.comboBoxCursos.Location = new System.Drawing.Point(149, 107);
+            this.comboBoxCursos.Name = "comboBoxCursos";
+            this.comboBoxCursos.Size = new System.Drawing.Size(275, 24);
+            this.comboBoxCursos.TabIndex = 2;
+            // 
+            // textBoxNotas
+            // 
+            this.textBoxNotas.Location = new System.Drawing.Point(149, 180);
+            this.textBoxNotas.Name = "textBoxNotas";
+            this.textBoxNotas.Size = new System.Drawing.Size(275, 22);
+            this.textBoxNotas.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nombre Alumno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cursos";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nota del curso";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Datos Alumnos";
+            // 
+            // buttonGuardar
+            // 
+            this.buttonGuardar.BackColor = System.Drawing.Color.Cyan;
+            this.buttonGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonGuardar.Location = new System.Drawing.Point(477, 42);
+            this.buttonGuardar.Name = "buttonGuardar";
+            this.buttonGuardar.Size = new System.Drawing.Size(111, 45);
+            this.buttonGuardar.TabIndex = 8;
+            this.buttonGuardar.Text = "Guardar Alumno";
+            this.buttonGuardar.UseVisualStyleBackColor = false;
+            // 
+            // buttonMostrar
+            // 
+            this.buttonMostrar.BackColor = System.Drawing.Color.Cyan;
+            this.buttonMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonMostrar.Location = new System.Drawing.Point(477, 200);
+            this.buttonMostrar.Name = "buttonMostrar";
+            this.buttonMostrar.Size = new System.Drawing.Size(111, 50);
+            this.buttonMostrar.TabIndex = 9;
+            this.buttonMostrar.Text = "Mostrar Alumnos";
+            this.buttonMostrar.UseVisualStyleBackColor = false;
+            // 
+            // buttonEliminar
+            // 
+            this.buttonEliminar.BackColor = System.Drawing.Color.LightCoral;
+            this.buttonEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEliminar.Location = new System.Drawing.Point(826, 200);
+            this.buttonEliminar.Name = "buttonEliminar";
+            this.buttonEliminar.Size = new System.Drawing.Size(94, 50);
+            this.buttonEliminar.TabIndex = 10;
+            this.buttonEliminar.Text = "Eliminar Alumno";
+            this.buttonEliminar.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.ClientSize = new System.Drawing.Size(988, 450);
+            this.Controls.Add(this.buttonEliminar);
+            this.Controls.Add(this.buttonMostrar);
+            this.Controls.Add(this.buttonGuardar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNotas);
+            this.Controls.Add(this.comboBoxCursos);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.dataGridViewDatos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Alumnos";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewDatos;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.ComboBox comboBoxCursos;
+        private System.Windows.Forms.TextBox textBoxNotas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button buttonGuardar;
+        private System.Windows.Forms.Button buttonMostrar;
+        private System.Windows.Forms.Button buttonEliminar;
     }
 }
 
